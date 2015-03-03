4c91dac7c2b9ea6b43ce8f07f2c838edc599a2ed
Mindaugas Azubalis <mindaugas.azubali@gmail.com>
2015-03-03 15:23:12 +0000

    user image added

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -24,8 +24,10 @@
 						<li><a href="/auth/register">Register</a></li>
 					@else
 						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><img src="/img/user.png">&nbsp&nbsp{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
+								
+								<li><a href="/auth/logout">Profile</a></li>
 								<li><a href="/auth/logout">Logout</a></li>
 							</ul>
 						</li>
